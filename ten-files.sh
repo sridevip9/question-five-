aeb9dd7fba92ecbae4c45b3b1de8e17b2e74619e
sridevi peddi <sridevip9@yahoo.com>
2016-10-12 21:16:35 -0500

    question-five

---
+++ ten-files.sh
@@ -0,0 +1,4 @@
+touch crumpets01.txt crumpets02.txt crumpets03.txt crumpets04.txt crumpets05.txt
+echo "The crags are the best part!"
+touch crumpets06.txt crumpets07.txt crumpets08.txt crumpets09.txt crumpets10.txt
+echo "My present working directory is: your-pwd-here"
